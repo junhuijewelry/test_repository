a6f1f26c4e82f7c27c538d7648b83b9136f8841c
junhuijewelry <junhuijewelry@163.com>
2016-01-18 16:08:57 +0800

    完善

--- src/main/java/com/info/serviceImpl/CourseServiceImpl.java
+++ src/main/java/com/info/serviceImpl/CourseServiceImpl.java
@@ -1,15 +1,20 @@
 package com.info.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.info.entity.CourseInfo;
+import com.info.mapper.CourseInfoMapper;
 import com.info.service.CourseService;
 
 public class CourseServiceImpl implements CourseService {
 
+	@Autowired
+	private CourseInfoMapper courseInfoMapper;
 
 	@Override
 	public CourseInfo selectCourse(Integer id) {
 		// TODO Auto-generated method stub
-		return null;
+		return courseInfoMapper.selectCourse(id);
 	}
 
 }
